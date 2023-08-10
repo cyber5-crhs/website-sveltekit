e6ad4ad00be5ee5d99f872752f98e3e89d32a31a
supercolbat <supercolbat@protonmail.com>
2023-08-09 22:55:17 -0500

    CSP config

--- svelte.config.js
+++ svelte.config.js
@@ -15,10 +15,21 @@ const config = {
 
     csp: {
       directives: {
+        'base-uri': ['self'],
+        'child-src': ['self'],
+        'connect-src': ['self'],
+        'img-src': ['self'],
+        'font-src': ['self'],
+        'form-action': ['self'],
+        'frame-ancestors': ['self'],
+        'frame-src': ['self'],
+        'manifest-src': ['self'],
+        'media-src': ['self'],
+        'object-src': ['none'],
+        'style-src': ['self'],
+        'default-src': ['self'],
         'script-src': ['self'],
-      },
-      reportOnly: {
-        'script-src': ['self'],
+        'worker-src': ['self'],
       },
     },
   },
